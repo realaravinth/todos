ae4112ead56b83c48c6bfdcb9a62c5b2bede9d8e
realaravinth <realaravinth@batsense.net>
2021-09-25 21:20:14 +0530

    todo functionality

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,20 +1,16 @@
+import logging
+import os
+
 from flask import Flask, render_template, session, request, redirect, url_for, make_response
 import sqlite3
 from flask_bcrypt import Bcrypt
 from flask import g
-
-DATABASE = 'databse.db'
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
-
-
-app = Flask(__name__)
-app.secret_key = b'14c9ddcac47f1659dc67771e61bd2158'
-bcrypt = Bcrypt(app)
 
 def init_db():
     with app.app_context():
@@ -23,17 +19,64 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+DATABASE = 'databse.db'
+app = Flask(__name__)
+app.secret_key = bytes(os.getenv('TODO_SECRET'), 'utf-8')
+app.logger.setLevel(logging.DEBUG)
+bcrypt = Bcrypt(app)
 
 init_db()
 
-@app.route("/")
+def add_task(task: str, username: str):
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO todo_tasks (name, user_id) VALUES (?, (SELECT ID from todo_users WHERE name = ?));', (task, username))
+    conn.commit()
+
+def get_tasks(username: str):
+    conn = get_db()
+    cur = conn.cursor()
+    tasks =  cur.execute('SELECT ID, name, done FROM todo_tasks WHERE user_id = (SELECT ID from todo_users WHERE name = ?)', [username]).fetchall()
+    return tasks
+
+@app.route("/", methods=["POST", "GET"])
 def index():
     if 'username' in session:
-        return render_template('index.html')
+        username = session["username"]
+        if request.method == 'POST':
+            task = str.strip(request.form['task'])
+            add_task(task, username)
+
+        tasks = get_tasks(username)
+        return render_template('index.html', tasks=tasks)
     else:
         return redirect(url_for('login'))
 
+@app.route("/task/<id>/done", methods=["POST", "GET"])
+def mark_done(id: int):
+    if 'username' in session:
+        username = session["username"]
+        conn = get_db()
+        cur = conn.cursor()
+        tasks =  cur.execute('UPDATE todo_tasks SET done = 1 WHERE ID = ?  AND user_id = (SELECT ID from todo_users WHERE name = ?);', [id, username])
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
 
+@app.route("/task/<id>/pending", methods=["POST", "GET"])
+def mark_pending(id: int):
+    if 'username' in session:
+        username = session["username"]
+        conn = get_db()
+        cur = conn.cursor()
+        cur.execute('UPDATE todo_tasks SET done = 0 WHERE ID = ? AND user_id = (SELECT ID from todo_users WHERE name = ?);', [id, username])
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/join", methods=["POST", "GET"])
 def join():
@@ -42,7 +85,6 @@ def join():
         username = str.strip(request.form['username'])
         password = str.strip(request.form['password'])
         confirm_password = str.strip(request.form['confirm_password'])
-
 
         if confirm_password != password:
             error = "Passwords don't match"
@@ -53,9 +95,6 @@ def join():
     else:
         return render_template('join.html', error=error)
 
-
-
-
 @app.route("/login", methods=["POST", "GET"])
 def login():
     error = None
@@ -63,8 +102,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         if validate_login(username, password):
-            location = format('/hello/%s' % (username))
-            resp = make_response(redirect(location))
+            resp = make_response(redirect(url_for('index')))
             session['username'] = username
             return resp
         else:
@@ -74,35 +112,25 @@ def login():
         return render_template('login.html', error=error)
 
 def validate_login(username: str, password: str) -> bool:
-    cur = get_db().cursor();
-    hash =  x = cur.execute('select hash from todo_users where name = ?', [username]).fetchone()[0]
-    if hash is not None and bcrypt.check_password_hash(hash, password):
-        return True
-
-    else:
-        return False
+    cur = get_db().cursor()
+    stored_hash = cur.execute('SELECT hash FROM todo_users WHERE name = ?', [username]).fetchone()[0]
+    return stored_hash is not None and bcrypt.check_password_hash(stored_hash, password)
 
 def register(username: str, password: str):
-    hash = bcrypt.generate_password_hash(password, rounds=12)
+    stored_hash = bcrypt.generate_password_hash(password, rounds=12)
     conn = get_db();
     cur = conn.cursor()
-    cur.execute('INSERT INTO todo_users (name, hash) VALUES (?, ?);', (username, hash))
+    cur.execute('INSERT INTO todo_users (name, hash) VALUES (?, ?);', (username, stored_hash))
     conn.commit()
     conn.close()
-    
 
 @app.route('/logout')
 def logout():
-    session('username', None)
+    session.pop('username', None)
     return redirect(url_for('index'))
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('index.html', name=name)
-
 @app.teardown_appcontext
-def close_connection(exception):
+def close_connection(_exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
